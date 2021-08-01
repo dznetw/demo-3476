d9fbf3eb1451538caa595afba5989110825f5388
DG <dz.network@pm.me>
2021-08-01 17:37:09 +0200

    Adds CarWorkshopRepository

---
+++ src/main/java/com/example/demo/repositories/CarWorkshopRepository.java
@@ -0,0 +1,15 @@
+package com.example.demo.repositories;
+
+import com.example.demo.model.CarWorkshop;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.Repository;
+import org.springframework.stereotype.Component;
+
+@Component
+public interface CarWorkshopRepository extends Repository<CarWorkshop, Long> {
+
+    // note: returning Stream might have better performance
+    @Query("SELECT c from CarWorkshops c where c.name=:workshopName")
+    Optional<CarWorkshop> findFirstByName(String workshopName);
+}
