a577665ca0fc4e9015102e5e45332aaf44f37186
DG <dz.network@pm.me>
2021-08-01 19:34:05 +0200

    Adds enhancement of AppointmentRepository for easier responses to API

--- src/main/java/com/example/demo/repositories/AppointmentRepository.java
+++ src/main/java/com/example/demo/repositories/AppointmentRepository.java
@@ -2,15 +2,24 @@ package com.example.demo.repositories;
 
 import com.example.demo.model.Appointment;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Component;
 
+// note: returning Streams might have better performance
 @Component
-public interface AppointmentRepository extends Repository<Appointment, Long> {
+public interface AppointmentRepository extends CrudRepository<Appointment, Long> {
 
-    // note: returning Stream might have better performance
-    @Query("SELECT a from Appointments a where a.workshop.name=:workshopName and a.date=:date")
+    @Query("SELECT a from Appointment a where a.workshop.name=:workshopName and a.date=:date")
     List<Appointment> findByWorkshopAndDate(String workshopName, LocalDate date);
+
+    @Query("SELECT a from Appointment a where a.id=:id and a.workshop.name=:workshopName")
+    Optional<Appointment> findAppointmentByIdAndWorkshop(Long id, String workshopName);
+
+    @Query("SELECT a from Appointment a where a.workshop.name=:workshopName and a.date=:date and a.start>=:from and a.end<=:until")
+    List<Appointment> findByWorkshopAndDateRange(String workshopName, LocalDate date, LocalTime from, LocalTime until);
+
 }
